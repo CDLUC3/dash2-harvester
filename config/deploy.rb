c524f433a1b91ddcc60f03ee9e5e5f50d2420372
David Moles <dmoles@cdl-dmoles-9m.ad.ucop.edu>
2016-04-20 15:32:33 -0700

    Initial Capistrano customization

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,13 +16,4 @@ set :deploy_to, '/apps/dash2/apps/dash2-harvester'
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
---
+++ config/deploy/development.rb
@@ -0,0 +1,1 @@
+server 'uc3-dash2harvest-dev.cdlib.org', user: 'dash2'
