caf9a158b6f3da81e0abf3bd62f942c5ddd58857
David Moles <david.moles@ucop.edu>
2016-04-21 17:07:49 -0700

    Rename logs/ to log/, and make it and db/ linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :deploy_to, '/apps/dash2/apps/dash2-harvester'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'db')
+
 # Default value for default_env is {}
 # set :default_env, { path: "/apps/dash2/apps/dash2-harvester/bin:$PATH" }
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 # Scheduling support, see https://github.com/javan/whenever
-set :output, '/apps/dash2/apps/dash2-harvester/current/logs/dash2-harvester.log'
+set :output, '/apps/dash2/apps/dash2-harvester/current/log/dash2-harvester.log'
 
 every 15.minutes do
   command "STASH_ENV=#{@environment} /apps/dash2/apps/dash2-harvester/current/bin/harvest.sh"
