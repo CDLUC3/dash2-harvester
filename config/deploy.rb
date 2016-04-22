76a2c104a71a5cacc7564d95a2dd5fb8eb543818
David Moles <dmoles@cdl-dmoles-9m.ad.ucop.edu>
2016-04-22 11:09:23 -0700

    Use /dash2 instead of /apps/dash2 to conform to role user's $HOME

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 
 set :application, 'dash2-harvester'
 set :repo_url, 'git@github.com:CDLUC3/dash2-harvester.git'
-set :deploy_to, '/apps/dash2/apps/dash2-harvester'
+set :deploy_to, '/dash2/dash2-harvester'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -11,7 +11,7 @@ set :deploy_to, '/apps/dash2/apps/dash2-harvester'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'db')
 
 # Default value for default_env is {}
-# set :default_env, { path: "/apps/dash2/apps/dash2-harvester/bin:$PATH" }
+# set :default_env, { path: "/dash2/dash2-harvester/bin:$PATH" }
 
 # Scheduling support, see https://github.com/javan/whenever
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # Scheduling support, see https://github.com/javan/whenever
-set :output, '/apps/dash2/apps/dash2-harvester/current/log/dash2-harvester.log'
+set :output, '/dash2/dash2-harvester/current/log/dash2-harvester.log'
 
 every 15.minutes do
-  command "STASH_ENV=#{@environment} /apps/dash2/apps/dash2-harvester/current/bin/harvest.sh"
+  command "STASH_ENV=#{@environment} /dash2/dash2-harvester/current/bin/harvest.sh"
 end
