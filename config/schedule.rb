7b9b8521c7881ba02be637f244127350111041fa
David Moles <david.moles@ucop.edu>
2016-05-24 12:15:43 -0500

    Increase harvester frequency to 5 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # Scheduling support, see https://github.com/javan/whenever
 set :output, '/dash2/apps/dash2-harvester/current/log/dash2-harvester.log'
 
-every 15.minutes do
+every 5.minutes do
   # TODO: configure environment in some less-hacky way
   command "STASH_ENV=#{ENV['STASH_ENV']} /dash2/apps/dash2-harvester/current/bin/harvest.sh"
 end
