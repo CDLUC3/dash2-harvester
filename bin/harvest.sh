d118f4ee7391bb180ac7413f4b76a47571eb64bc
David Moles <david.moles@ucop.edu>
2016-11-09 13:51:26 -0800

    Look for stopfile in shared, not current

--- bin/harvest.sh
+++ bin/harvest.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 APP_DIR=$(realpath -s `dirname $0`/..)
-cd $APP_DIR
-
 CONFIG_FILE=${APP_DIR}/config/stash-harvester.yml
-STOP_FILE=${APP_DIR}/config/stash-harvester.stop
+
+SHARED_DIR=$(realpath ${APP_DIR}/log/..)
+STOP_FILE=${SHARED_DIR}/stash-harvester.stop
+
+cd $APP_DIR
 bundle exec stash-harvester -c ${CONFIG_FILE} -s ${STOP_FILE}
